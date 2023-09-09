4845b536ce5ef0513298c0d3f8ef188a4bb5f303
Marion Buchner <marionbuechner@MACBOOK-AIR.station>
2023-09-09 10:05:03 +0100

    d1p1 directory count is not matching - debug it

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ def main():
         current_dir = ''
         directories= dict()
         current_size = 0
+        dir_count = 0
 
         for line in f:
             print(f'line is : {line}')
             l = line.split(' ')
-            # print(f'l 0 is {l[0]}')
-            match = re.match('cd$', l[1])
-            if(match != None):
-                # print(f'entered directory : {l[2]}')
-                # ARE WE MISSING The last itme like this?
+            if(re.match('cd$', l[1])):
+                dir_count += 1
+                print(f'found dir {l[2]}')
                 if(current_dir):
                     directories[current_dir] = current_size
                 current_dir = l[2]
@@ -29,6 +28,8 @@ def main():
 
         for k in directories:
             print(f'{k} size == {directories[k]}')
+        print(len(directories.keys()))
+        print(dir_count)
         print('done')
 
 

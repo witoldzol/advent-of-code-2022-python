778d78af41b82625e754f7dea0cfaefec4cfd73a
Marion Buchner <marionbuechner@MACBOOK-AIR.station>
2023-09-09 09:23:16 +0100

    d7p1 get all directories and file sizes, no recursion

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
+import re
+
+
 def main():
-    print('hi')
+    with open('input') as f:
+        current_dir = ''
+        directories= dict()
+        current_size = 0
+
+        for line in f:
+            print(f'line is : {line}')
+            l = line.split(' ')
+            # print(f'l 0 is {l[0]}')
+            match = re.match('cd$', l[1])
+            if(match != None):
+                # print(f'entered directory : {l[2]}')
+                # ARE WE MISSING The last itme like this?
+                if(current_dir):
+                    directories[current_dir] = current_size
+                current_dir = l[2]
+                current_size = 0
+
+            if(re.match('^[0-9]+$', l[0])):
+                print(f'found file of size : {l[0]}')
+                current_size += int(l[0])
+
+        if(current_dir):
+            directories[current_dir] = current_size
+
+        for k in directories:
+            print(f'{k} size == {directories[k]}')
+        print('done')
+
 
 main()

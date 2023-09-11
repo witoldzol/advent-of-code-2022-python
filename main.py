bc3eb771f841687c9f3b6213de0a3cff7130ccac
Witold Zolnowski <blue3656@gmail.com>
2023-09-11 21:03:24 +0100

    d7p1 reformat

--- main.py
+++ main.py
@@ -2,20 +2,22 @@ import re
 
 from typing import List, Dict
 
-class Directory():
-    def __init__(self, name: str, parent = None):
+
+class Directory:
+    def __init__(self, name: str, parent=None):
         self.name = name
-        self.parent: 'Directory'| None = parent
-        self.dirs: List['Directory'] = []
+        self.parent: "Directory" | None = parent
+        self.dirs: List["Directory"] = []
         self.files: List[Dict[str, int]] = []
         self.size: int = 0
-    
+
     def depth_first_traversal(self, n: int = 0):
-        tabs = '|' * n
+        tabs = "|" * n
         n += 1
         print(tabs + self.name)
         for d in self.dirs:
             d.depth_first_traversal(n)
+
     def sum_files(self):
         sum = 0
         for f in self.files:
@@ -29,34 +31,32 @@ class Directory():
         return sum
 
 
-
-
 def main():
-    root = create_tree('input')
+    root = create_tree("input")
     root.depth_first_traversal()
-    print('sum is : ' + str(root.get_sum()))
+    print("sum is : " + str(root.get_sum()))
 
 
 def create_tree(file: str) -> Directory:
     with open(file) as f:
         counter = 0
-        root = Directory('/', None)
+        root = Directory("/", None)
         current_dir = root
         for line in f:
             line = line.rstrip()
             # print(f'current line {line}')
-            EXIT_DIR = re.match(r'^\$ cd \.\.$', line)
-            GO_TO_ROOT = re.match(r'^\$ cd \/$', line)
-            GO_TO_DIR = re.match(r'^\$ cd \w+$', line)
-            LIST_DIR = re.match(r'^\$ ls$', line)
-            DIR = re.match(r'^dir \w+', line)
-            FILE = re.match(r'^\d+ \w+', line)
-            if(EXIT_DIR):
+            EXIT_DIR = re.match(r"^\$ cd \.\.$", line)
+            GO_TO_ROOT = re.match(r"^\$ cd \/$", line)
+            GO_TO_DIR = re.match(r"^\$ cd \w+$", line)
+            LIST_DIR = re.match(r"^\$ ls$", line)
+            DIR = re.match(r"^dir \w+", line)
+            FILE = re.match(r"^\d+ \w+", line)
+            if EXIT_DIR:
                 current_dir = current_dir.parent
-            elif(GO_TO_DIR):
-                #print('matched go to dir')
+            elif GO_TO_DIR:
+                # print('matched go to dir')
                 dir_name = line.split()[2]
-                #print(f'dir name is {repr(dir_name)}')
+                # print(f'dir name is {repr(dir_name)}')
                 temp = current_dir
                 for d in current_dir.dirs:
                     if d.name == dir_name:
@@ -64,28 +64,29 @@ def create_tree(file: str) -> Directory:
                         break
                 if temp == current_dir:
                     raise Exception("Failed to find child folder")
-            elif(LIST_DIR):
-                #print('matched ls ')
+            elif LIST_DIR:
+                # print('matched ls ')
                 pass
-            elif(DIR):
-                #print('matched a directory')
+            elif DIR:
+                # print('matched a directory')
                 dir_name = line.split()[1]
                 d = Directory(dir_name, current_dir)
                 current_dir.dirs.append(d)
                 counter += 1
-            elif(FILE):
-                #print('matched a file')
+            elif FILE:
+                # print('matched a file')
                 size = int(line.split()[0])
                 file_name = line.split()[1]
                 current_dir.files.append({file_name: size})
-            elif(GO_TO_ROOT):
-                #print('matched go to root')
+            elif GO_TO_ROOT:
+                # print('matched go to root')
                 continue
             else:
-                raise Exception('Invalid input')
+                raise Exception("Invalid input")
 
-        print(f' conter : = {counter}')
+        print(f" conter : = {counter}")
         return root
+
 
 if __name__ == "__main__":
     main()

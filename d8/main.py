9c43deef50eac8a10b78e4416a32467499b56b2a
Witold Zolnowski <blue3656@gmail.com>
2023-09-13 17:07:28 +0100

    d8p1 udpate edge detection

--- d8/main.py
+++ d8/main.py
@@ -8,19 +8,23 @@ def main():
             s = list(l.rstrip())
             forest.append(s)
 
-def is_edge_tree(x: int):
-    return x-1 < 0
+def is_edge_tree(x: int, y: int , lx: int, ly: int):
+    return x == 0 or \
+           y == 0 or \
+           x+1 >= lx or \
+           y+1 >= ly
 
 def count_visible_trees(forest: List[List[int]]) -> int:
     count = 0
     for x, row in enumerate(forest):
         for y, tree in enumerate(row):
             # top
-            if is_edge_tree(x):
+            if is_edge_tree(x, y, len(forest), len(forest[0])):
                 count += 1
                 continue
             tree_above = forest[x-1][y]
-            if tree_above > tree:
+            tree_below = forest[x+1][y]
+            if tree_above > tree and tree_below > tree:
                 print(f'tree {tree} [{x},{y}] is obscured by tree above {tree_above}')
                 continue
             count += 1
--- d8/test_forest.py
+++ d8/test_forest.py
@@ -18,3 +18,21 @@ def test_three_by_three_trees_one_hidden():
               [1,0,1],
               [2,2,2]]
     assert count_visible_trees(forest) == 8
+
+def test_four_by_four_trees_two_hidden():
+    forest = [[1,1,1,1],
+              [1,0,0,1],
+              [2,2,2,2]]
+    assert count_visible_trees(forest) == 10 
+
+def test_four_by_four_trees_one_hidden():
+    forest = [[1,1,1,1],
+              [1,0,2,1],
+              [2,2,2,2]]
+    assert count_visible_trees(forest) == 11 
+
+def test_four_by_four_trees_one_hidden_bottom():
+    forest = [[1,1,2,1],
+              [1,0,2,1],
+              [2,2,1,2]]
+    assert count_visible_trees(forest) == 11 

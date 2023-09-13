c61807c2c41737f631e04d7bb1926c32209c5911
Witold Zolnowski <blue3656@gmail.com>
2023-09-13 12:54:45 +0100

    d8p1 test tree above

--- d8/main.py
+++ d8/main.py
@@ -7,16 +7,24 @@ def main():
         for l in f:
             s = list(l.rstrip())
             forest.append(s)
-        # x_max = len(forest)
-        # y_max = len(forest[0])
-        # visible_trees_count = 0
-        # for x, row in enumerate(forest):
-        #     for y, tree in enumerate(row):
-        #         pass
 
+def is_edge_tree(x: int):
+    return x-1 < 0
 
 def count_visible_trees(forest: List[List[int]]) -> int:
-    return 1
+    count = 0
+    for x, row in enumerate(forest):
+        for y, tree in enumerate(row):
+            # top
+            if is_edge_tree(x):
+                count += 1
+                continue
+            tree_above = forest[x-1][y]
+            if tree_above > tree:
+                print(f'tree {tree} [{x},{y}] is obscured by tree above {tree_above}')
+                continue
+            count += 1
+    return count
 
 
 if __name__ == "__main__":
--- d8/test_forest.py
+++ d8/test_forest.py
@@ -5,3 +5,16 @@ def test_one_tree():
     forest = [[1]]
     assert count_visible_trees(forest) == 1
 
+def test_two_trees():
+    forest = [[1],[2]]
+    assert count_visible_trees(forest) == 2
+
+def test_two_by_two_trees():
+    forest = [[1,1],[2,2]]
+    assert count_visible_trees(forest) == 4
+
+def test_three_by_three_trees_one_hidden():
+    forest = [[1,1,1],
+              [1,0,1],
+              [2,2,2]]
+    assert count_visible_trees(forest) == 8

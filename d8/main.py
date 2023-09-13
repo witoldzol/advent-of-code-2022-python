6de3c61e95f441aa399ffa51500f112e65b3e673
Witold Zolnowski <blue3656@gmail.com>
2023-09-13 17:51:21 +0100

    d8p1 detect all trees above

--- d8/main.py
+++ d8/main.py
@@ -16,19 +16,33 @@ def is_edge_tree(x: int, y: int , lx: int, ly: int):
            x+1 >= lx or \
            y+1 >= ly
 
+def is_higher_above(forest: List[List[int]], tree: int, x: int, y: int) -> bool:
+    for t in range(x):
+        if forest[t][y] >= tree:
+            return True
+    return False
+
 def count_visible_trees(forest: List[List[int]]) -> int:
     count = 0
     for x, row in enumerate(forest):
         for y, tree in enumerate(row):
             # top
+            # print(f'you are here => {x},{y}')
+            # print(f'tree => {tree}')
             if is_edge_tree(x, y, len(forest), len(forest[0])):
                 count += 1
                 continue
-            tree_above = forest[x-1][y]
+
+    # 30373
+    # 25512
+    # 65332
+    # 33549
+    # 35390
+            tree_above = is_higher_above(forest, tree, x , y)
             tree_below = forest[x+1][y]
             tree_left = forest[x][y-1]
             tree_right = forest[x][y+1]
-            if tree_above >= tree and tree_below >= tree and tree_left >= tree and tree_right >= tree:
+            if tree_above and tree_below >= tree and tree_left >= tree and tree_right >= tree:
                 print(f'tree {tree} [{x},{y}] is obscured by tree above {tree_above} \n \
                       and below {tree_below} \n \
                       and to the left {tree_left} \n \

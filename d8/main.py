1606c29c37e2bf98a809efdcd098fa1e56ba196c
Marion Buchner <marionbuechner@MACBOOK-AIR.station>
2023-09-14 08:11:58 +0100

    d8p2 part 2 solved ( tests not updated )

--- d8/main.py
+++ d8/main.py
@@ -17,49 +17,68 @@ def is_edge_tree(x: int, y: int , lx: int, ly: int):
            x+1 >= lx or \
            y+1 >= ly
 
-def is_higher_below(forest: List[List[int]], tree: int, x: int, y: int) -> bool:
+def is_higher_below(forest: List[List[int]], tree: int, x: int, y: int) -> int:
+    count = 0
     for t in range(x+1, len(forest)):
+        if forest[t][y] < tree:
+            count += 1
         if forest[t][y] >= tree:
-            return True
-    return False
+            count += 1
+            break
+    return count
 
-def is_higher_above(forest: List[List[int]], tree: int, x: int, y: int) -> bool:
-    for t in range(0, x):
+def is_higher_above(forest: List[List[int]], tree: int, x: int, y: int) -> int:
+    count = 0
+    for t in range(x-1, -1, -1):
+        if forest[t][y] < tree:
+            count += 1
         if forest[t][y] >= tree:
-            return True
-    return False
+            count += 1
+            break
+    return count
 
-def is_higher_left(forest: List[List[int]], tree: int, x: int, y: int) -> bool:
-    for t in range(0, y):
+def is_higher_left(forest: List[List[int]], tree: int, x: int, y: int) -> int:
+    count = 0
+    for t in range(y-1, -1, -1):
+        if forest[x][t] < tree:
+            count += 1
         if forest[x][t] >= tree:
-            return True
-    return False
+            count += 1
+            break
+    return count
 
-def is_higher_right(forest: List[List[int]], tree: int, x: int, y: int) -> bool:
+def is_higher_right(forest: List[List[int]], tree: int, x: int, y: int) -> int:
+    count = 0
     for t in range(y+1, len(forest)):
+        if forest[x][t] < tree:
+            count += 1
         if forest[x][t] >= tree:
-            return True
-    return False
+            count += 1
+            break
+    return count
 
 def count_visible_trees(forest: List[List[int]]) -> int:
-    count = 0
+    max = 0
     for x, row in enumerate(forest):
         for y, tree in enumerate(row):
             if is_edge_tree(x, y, len(forest), len(forest[0])):
-                count += 1
                 continue
+            print('=======================')
+            print(f' tree {x},{y} ')
             tree_above = is_higher_above(forest, tree, x , y)
+            print(f'above = {tree_above}')
             tree_below = is_higher_below(forest, tree, x , y)
+            print(f'below = {tree_below}')
             tree_left = is_higher_left(forest, tree, x , y) 
+            print(f'left = {tree_left}')
             tree_right = is_higher_right(forest, tree, x , y) 
-            if tree_above and tree_below and tree_left and tree_right:
-                print(f'tree {tree} [{x},{y}] is obscured by tree above {tree_above} \n \
-                      and below {tree_below} \n \
-                      and to the left {tree_left} \n \
-                      and to the right {tree_right}')
-            else:
-                count += 1
-    return count
+            print(f'right = {tree_right}')
+            scenic_score =  tree_above * tree_below * tree_left * tree_right
+            print('=======================')
+            if scenic_score > max:
+                max = scenic_score
+    return max
+
 
 if __name__ == "__main__":
     main('input')

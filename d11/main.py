ce36681b883cf3c9307f81410c4a121d2f23a912
Witold Zolnowski <blue3656@gmail.com>
2023-09-17 16:27:22 +0100

    d11p1 add test for sample input

--- d11/main.py
+++ d11/main.py
@@ -10,6 +10,7 @@ class Monkey():
     test: int
     test_true: int
     test_false: int
+    inspect_counter: int = 0
 
     def __init__(self) -> None:
         self.items = []
@@ -27,18 +28,25 @@ class Monkey():
                 raise Exception(f'Found unknown operation {self.operation[1]}')
 
 
-def main(file):
+def main(file) -> int:
     monkeys: List[Monkey] = parse_monkeys_from_input(file)
+    for _ in range(20):
+        for m in monkeys:
+            for i in m.items:
+                m.inspect_counter += 1
+                new_i = math.floor(m.recalculate_item_worry(i) / 3)
+                if new_i % m.test == 0:
+                    monkeys[m.test_true].items.append(new_i)
+                else:
+                    monkeys[m.test_false].items.append(new_i)
+            m.items.clear()
     for m in monkeys:
-        print('monkey has items :', m.items)
-        for i in m.items:
-            print('item : ' , i)
-            new_i = math.floor(m.recalculate_item_worry(i) / 3)
-            if new_i % m.test == 0:
-                monkeys[m.test_true].items.append(new_i)
-            else:
-                monkeys[m.test_false].items.append(new_i)
-        m.items.clear()
+        print(m.inspect_counter)
+    counters = [m.inspect_counter for m in monkeys ]
+    highest_two = sorted(counters)[-2:]
+    result = highest_two[0] * highest_two[1]
+    print('Solution is : ', result)
+    return  result
 
 
 def parse_monkeys_from_input(file) -> List[Monkey]:
@@ -98,4 +106,4 @@ def parse_monkeys_from_input(file) -> List[Monkey]:
 
 
 if __name__ == "__main__":
-    main('sample_input')
+    main('input')

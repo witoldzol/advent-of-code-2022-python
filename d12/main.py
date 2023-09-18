eb72838854983538751b283d82f76ac0953eb196
Witold Zolnowski <blue3656@gmail.com>
2023-09-18 21:17:41 +0100

    d12p1 add mock test

--- d12/main.py
+++ d12/main.py
@@ -2,6 +2,7 @@ def main(filename):
     with open(filename, 'r') as f:
         for line in f:
             print(line.rstrip())
+    return 31
 
 if __name__ == "__main__":
     main('sample_input')
---
+++ d12/test.py
@@ -0,0 +1,5 @@
+from main import main
+
+
+def test_sample_input():
+    assert main('sample_input') == 31

f4491611fb09b1243689ccf92f6948ca5ace19f8
Witold Zolnowski <blue3656@gmail.com>
2023-09-18 21:37:57 +0100

    d12p1 build matrix from input

--- d12/main.py
+++ d12/main.py
@@ -1,8 +1,24 @@
+from typing import List
+
+
 def main(filename):
+    # check all adjecent cells
+    # loop over valid ones 
+    # each call will be recursive
+    # pass location from where you came, to avoid backtracking
+    start = [0,0]
+    end = [2,5]
+    matrix = build_matrix(filename) 
+    print(matrix)
+    return 31
+
+def build_matrix(filename: str) -> List['str']:
+    matrix = []
     with open(filename, 'r') as f:
         for line in f:
-            print(line.rstrip())
-    return 31
+            line = line.rstrip()
+            matrix.append(list(line))
+    return matrix
 
 if __name__ == "__main__":
     main('sample_input')

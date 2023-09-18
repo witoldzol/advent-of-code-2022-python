6986cb6f4167c88ad053c4ed5655abf5cea37986
Witold Zolnowski <blue3656@gmail.com>
2023-09-18 21:46:35 +0100

    d12p1 add direction namedtuple

--- d12/main.py
+++ d12/main.py
@@ -1,16 +1,15 @@
+from collections import namedtuple
 from typing import List
 
+Direction = namedtuple('Direction', "x y") 
 
 def main(filename):
-    # check all adjecent cells
-    # loop over valid ones 
-    # each call will be recursive
-    # pass location from where you came, to avoid backtracking
-    start = [0,0]
-    end = [2,5]
+    start = Direction(0,0)
+    end = Direction(2,5)
     matrix = build_matrix(filename) 
     print(matrix)
     return traverse(matrix, start, end)
+
 
 def build_matrix(filename: str) -> List[List[str]]:
     matrix = []
@@ -21,8 +20,20 @@ def build_matrix(filename: str) -> List[List[str]]:
     return matrix
 
 
-def traverse(matrix: List[List[str]], start: List[int], end: List[int]) -> int:
+def traverse(matrix: List[List[str]], start: Direction, end: Direction) -> int:
+    # check all adjecent cells
+    # loop over valid ones 
+    # each call will be recursive
+    # pass location from where you came, to avoid backtracking
+    directions: List[Direction] = get_valid_directions(matrix, start)
     return 31
+
+
+def get_valid_directions(matrix: List[List[str]], start: Direction) -> List[Direction]:
+    print(start.x)
+    print(start.y)
+    return list(Direction(1,1))
+
 
 if __name__ == "__main__":
     main('sample_input')

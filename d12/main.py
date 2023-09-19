d4883602dee668415c73356efabc7b9b7dc5a3d5
Witold Zolnowski <blue3656@gmail.com>
2023-09-19 11:36:04 +0100

    d12p1 evaluate surrounding cells

--- d12/main.py
+++ d12/main.py
@@ -30,8 +30,43 @@ def traverse(matrix: List[List[str]], start: Direction, end: Direction) -> int:
 
 
 def get_valid_directions(matrix: List[List[str]], start: Direction) -> List[Direction]:
-    print(start.x)
-    print(start.y)
+    direction = 'UP'
+    up = start.x - 1
+    if up < 0:
+        print(f'Going {direction} to {up},{start.y} is off bounds')
+    else:
+        if matrix[up][start.y] > matrix[start.x][start.y]:
+            print(f'start: {start.x},{start.y} is lower than {direction} {up}{start.y}')
+        else:
+            print(f'start: {start.x},{start.y} is higher or equal than {direction} {up},{start.y}')
+    direction = 'DOWN'
+    down = start.x + 1
+    if down < 0:
+        print(f'Going {direction} to {down},{start.y} is off bounds')
+    else:
+        if matrix[down][start.y] > matrix[start.x][start.y]:
+            print(f'start: {start.x},{start.y} is lower than {direction} {down}{start.y}')
+        else:
+            print(f'start: {start.x},{start.y} is higher or equal than {direction} {down},{start.y}')
+    direction = 'LEFT'
+    left = start.y - 1
+    if up < 0:
+        print(f'Going {direction} to {start.x},{left} is off bounds')
+    else:
+        if matrix[start.x][left] > matrix[start.x][start.y]:
+            print(f'start: {start.x},{start.y} is lower than {direction} {start.x}{left}')
+        else:
+            print(f'start: {start.x},{start.y} is higher or equal than {direction} {start.x},{left}')
+    direction = 'RIGHT'
+    right = start.y + 1
+    if up < 0:
+        print(f'Going {direction} to {start.x},{right} is off bounds')
+    else:
+        if matrix[start.x][right] > matrix[start.x][start.y]:
+            print(f'start: {start.x},{start.y} is lower than {direction} {start.x}{right}')
+        else:
+            print(f'start: {start.x},{start.y} is higher or equal than {direction} {start.x},{right}')
+
     return list(Direction(1,1))
 
 

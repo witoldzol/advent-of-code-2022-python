2d60a02cf36d3e6cf6ba228d60c4f7ca4d9d8942
Witold Zolnowski <blue3656@gmail.com>
2023-09-26 13:32:50 +0100

    d12p1 use offsets to remove duplication, reformat

--- d12/main.py
+++ d12/main.py
@@ -1,39 +1,39 @@
 from typing import List, Dict, Tuple
 from queue import Queue
 
+
 class Cell:
     x: int
     y: int
     v: int
-    
-    def __init__(self, x:int, y:int, v:str):
+
+    def __init__(self, x: int, y: int, v: str):
         self.x = x
         self.y = y
         self.v = ord(v)
 
     def __str__(self) -> str:
-        return f'{self.x},{self.y}'
-
+        return f"{self.x},{self.y}"
 
     def __repr__(self) -> str:
-        return f'Cell(x={self.x},y={self.y},v={self.v})'
+        return f"Cell(x={self.x},y={self.y},v={self.v})"
 
 
 def main(filename):
-    matrix = build_matrix(filename) 
+    matrix = build_matrix(filename)
     start = find_start(matrix)
-    start_cell = Cell(start[0],start[1],'a')
+    start_cell = Cell(start[0], start[1], "a")
     path = breadth_traverse(matrix, start_cell)
-    print(f'steps => {len(path)}')
+    print(f"steps => {len(path)}")
     visualise_path(len(matrix), len(matrix[0]), path)
 
 
-def find_start(matrix: List[List[str]]) -> Tuple[int,int]:
+def find_start(matrix: List[List[str]]) -> Tuple[int, int]:
     for x, row in enumerate(matrix):
         for y, cell in enumerate(row):
-            if cell == 'S':
-                return x,y
-    return 0,0
+            if cell == "S":
+                return x, y
+    return 0, 0
 
 
 def visualise_path(x: int, y: int, path: List[str]) -> None:
@@ -41,18 +41,19 @@ def visualise_path(x: int, y: int, path: List[str]) -> None:
     for _ in range(x):
         temp = []
         for _ in range(y):
-            temp.append('.')
+            temp.append(".")
         matrix.append(temp)
     for cell in path:
-        x,y = cell.split(',')
-        matrix[int(x)][int(y)] = '='
+        x, y = cell.split(",")
+        matrix[int(x)][int(y)] = "="
     for row in matrix:
-        r = ''.join(row)
+        r = "".join(row)
         print(r)
+
 
 def build_matrix(filename: str) -> List[List[str]]:
     matrix = []
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         for line in f:
             line = line.rstrip()
             matrix.append(list(line))
@@ -60,91 +61,78 @@ def build_matrix(filename: str) -> List[List[str]]:
 
 
 def is_end(matrix, d):
-    if matrix[d.x][d.y] == 'E':
-        print(f'Found ending at {d.x},{d.y}')
+    if matrix[d.x][d.y] == "E":
+        print(f"Found ending at {d.x},{d.y}")
         return True
-    
+
 
 def breadth_traverse(matrix: List[List[str]], start_cell: Cell) -> List[str]:
     path: List[str] = []
     count = 0
-    visited: Dict[str,bool] = {}
+    visited: Dict[str, bool] = {}
     next_to_visit: Queue = Queue()
     valid_directions: List[Cell] = get_valid_directions(matrix, visited, start_cell)
     # init queue
     for d in valid_directions:
         next_to_visit.put(d)
     parent_map = {}
-    for d in iter(next_to_visit.get, None):
-        cell = Cell(d.x,d.y,matrix[d.x][d.y])
-        parent = d
+    for cell in iter(next_to_visit.get, None):
         count += 1
         if str(cell) in visited:
             continue
         visited[str(cell)] = True
         valid_directions: List[Cell] = get_valid_directions(matrix, visited, cell)
         # check if end found
-        for d in valid_directions:
-            if is_end(matrix, d):
-                parent_map[str(d)] = str(parent)
-                path = trace_back_path(d, parent_map)
+        for child in valid_directions:
+            if is_end(matrix, child):
+                parent_map[str(child)] = str(cell)
+                path = trace_back_path(child, parent_map)
                 return path
-            if str(d) not in visited:
-                parent_map[str(d)] = str(parent)
-                next_to_visit.put(d)
+            if str(child) not in visited:
+                parent_map[str(child)] = str(cell)
+                next_to_visit.put(child)
     return path
 
 
-def trace_back_path(end: Cell, map: Dict[str,str]) -> List[str]:
+def trace_back_path(end: Cell, map: Dict[str, str]) -> List[str]:
     path = []
     path.append(str(end))
     current = str(end)
     while current in map:
         parent: str = map[current]
         path.append(parent)
-        current = parent 
+        current = parent
     return path
 
 
 def translate_start_and_end_cells(neighbour_cell: str) -> str:
-    if neighbour_cell == 'E':
-        neighbour_cell = 'z'
-    if neighbour_cell == 'S':
-        neighbour_cell = '|' # | is the 2 chars after z, and will always be 'higher than start cell ( hence S will never be valid direction)
+    if neighbour_cell == "E":
+        neighbour_cell = "z"
+    if neighbour_cell == "S":
+        neighbour_cell = "|"  # | is the 2 chars after z, and will always be 'higher than start cell ( hence S will never be valid direction)
     return neighbour_cell
 
 
-def get_valid_directions(matrix: List[List[str]], visited: Dict[str,bool], start: Cell) -> List[Cell]:
+def is_out_of_bounds(offset: List[int], len_x: int, len_y: int) -> bool:
+    if offset[0] < 0 or offset[1] < 0:
+        return True
+    if offset[0] >= len_x or offset[1] >= len_y:
+        return True
+    return False
+
+
+def get_valid_directions(
+    matrix: List[List[str]], visited: Dict[str, bool], start: Cell
+) -> List[Cell]:
     valid_directions: List[Cell] = []
-    up = start.x - 1
-    if not up < 0:
-        neighbour_cell = matrix[up][start.y]  
-        neighbour_cell = translate_start_and_end_cells(neighbour_cell)
-        target_cell: Cell = Cell(up,start.y,neighbour_cell)
-        if target_cell.v <= (start.v + 1):
-            if str(target_cell) not in visited:
-                valid_directions.append(target_cell)
-    down = start.x + 1
-    if not down >= len(matrix):
-        neighbour_cell = matrix[down][start.y]  
-        neighbour_cell = translate_start_and_end_cells(neighbour_cell)
-        target_cell: Cell = Cell(down,start.y,neighbour_cell)
-        if target_cell.v <= (start.v + 1):
-            if str(target_cell) not in visited:
-                valid_directions.append(target_cell)
-    left = start.y - 1
-    if not left < 0:
-        neighbour_cell = matrix[start.x][left]  
-        neighbour_cell = translate_start_and_end_cells(neighbour_cell)
-        target_cell: Cell = Cell(start.x,left,neighbour_cell)
-        if target_cell.v <= (start.v + 1):
-            if str(target_cell) not in visited:
-                valid_directions.append(target_cell)
-    right = start.y + 1
-    if not right >= len(matrix[0]):
-        neighbour_cell = matrix[start.x][right]  
-        neighbour_cell = translate_start_and_end_cells(neighbour_cell)
-        target_cell: Cell = Cell(start.x,right,neighbour_cell)
+    offsets = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    for i, k in offsets:
+        offset = [start.x + i, start.y + k]
+        if is_out_of_bounds(offset, len(matrix), len(matrix[0])):
+            continue
+        cell_value = matrix[offset[0]][offset[1]]
+        cell_value = translate_start_and_end_cells(cell_value)
+        target_cell: Cell = Cell(offset[0], offset[1], cell_value)
         if target_cell.v <= (start.v + 1):
             if str(target_cell) not in visited:
                 valid_directions.append(target_cell)
@@ -152,5 +140,5 @@ def get_valid_directions(matrix: List[List[str]], visited: Dict[str,bool], start
 
 
 if __name__ == "__main__":
-   # main('sample_input')
-   main('input')
+    # main('sample_input')
+    main("input")

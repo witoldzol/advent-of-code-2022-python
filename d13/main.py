9bb68ffe15d1e2eb6e9c2516c0126fa15ecf9802
Witold Zolnowski <blue3656@gmail.com>
2023-10-10 07:24:29 +0100

    d13,p2 replace bools with standard comparison ints

--- d13/main.py
+++ d13/main.py
@@ -17,7 +17,7 @@ def main(filename):
             p2 = eval(p2)
             print(f"[START]  LEFT = {p1} and RIGHT = {p2}")
             print(f'INDEX = {i}')
-            if compare(p1,p2) == True:
+            if compare(p1,p2) == -1:
                 result += 1 + i
         print(f'RESULT is {result}')
 
@@ -27,11 +27,11 @@ def compare(p1: int|List, p2):
     print(f"[compare2]  LEFT = {p1} and RIGHT = {p2}")
     if isinstance(p1, int) and isinstance(p2, int):
         if p1 > p2:
-            return False 
+            return 1 
         elif p2 > p1:
-            return True
+            return -1
         else:
-            return None
+            return 0
     # both are lists
     elif isinstance(p1, list) and isinstance(p2, list):
         i = 0
@@ -39,20 +39,20 @@ def compare(p1: int|List, p2):
             print(f'Im in a loop, comparing {p1[i]} and {p2[i]}')
             c = compare(p1[i],p2[i])
             i += 1
-            if  c == True:
-                return True
-            elif c == False:
-                return False
+            if  c == -1:
+                return -1
+            elif c == 1:
+                return 1
             # if items are equal, continue loop
-        # if left out of items, return True
+        # if left out of items, return -1
         if i == len(p1) and i < len(p2):
-            return True
-        # if right out of items, return False
+            return -1
+        # if right out of items, return 1
         elif i == len(p2) and i < len(p1):
-            return False
-        # if both are  equal, return None
+            return 1
+        # if both are  equal, return 0
         else:
-            return None
+            return 0
     elif isinstance(p1, int) and isinstance(p2, list):
         return compare([p1],p2)
     else:

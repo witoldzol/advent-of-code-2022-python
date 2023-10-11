a0cf102b0d12aaf4324e9bac7ab301a2b15400c9
Witold Zolnowski <blue3656@gmail.com>
2023-10-11 07:58:45 +0100

    d14,1 temp - drawing cave

--- d14/main.py
+++ d14/main.py
@@ -1,3 +1,4 @@
+import sys
 from typing import List
 
 def main(filename):
@@ -16,6 +17,21 @@ def get_coords(filename: str) -> List[List[int]]:
     return result
 
 def draw_cave(coords: List[List[int]]) -> List[List[str]]:
+    min_x = sys.maxsize
+    min_y = 0
+    max_x = -1
+    max_y = -1
+    for c in coords:
+        x, y = c
+        if x < min_x:
+            min_x = x
+        elif x > max_x:
+            max_x = x
+        if y < min_y:
+            min_y = y
+        elif y > max_y:
+            max_y = y
+    for  # todo finish drawing a cave 2d matrix
     return [[]]
 
 if __name__ == "__main__":

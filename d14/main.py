3c8e4561fade0d9a0c2e0e5913b358b15bf2a5db
Witold Zolnowski <blue3656@gmail.com>
2023-10-11 07:45:40 +0100

    d14,1 get_coords from input

--- d14/main.py
+++ d14/main.py
@@ -1,5 +1,18 @@
+from typing import List
+
 def main(filename):
-    print(filename)
+    coords = get_coords(filename)
+    print(coords)
+
+def get_coords(filename: str) -> List[List[int]]:
+    with open(filename, 'r') as input:
+        result = []
+        for line in input:
+            coords = line.strip().split('->')
+            for c in coords:
+                out = [int(x) for x in c.split(',')]
+                result.append(out)
+    return result
 
 
 if __name__ == "__main__":

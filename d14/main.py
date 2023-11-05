b685948582e811669bf200b7dcade9bb62526702
Witold Zolnowski <blue3656@gmail.com>
2023-11-05 10:12:16 +0000

    d14,1 clean up

--- d14/main.py
+++ d14/main.py
@@ -5,11 +5,7 @@ from typing import List, Tuple
 def main(filename):
     coords = get_coords(filename)
     cave = draw_cave(coords)
-    # x_labels = [f"{i}" for i in range(len(cave))]
-    # print('[x]', end='')
-    # print(x_labels)
-    for i,row in enumerate(cave):
-        # print(f"[{i}]", end='')
+    for row in cave:
         print(row)
 
 
@@ -52,9 +48,6 @@ def draw_cave(coords: List[List[List[int]]]) -> List[List[int]]:
                 start_x,end_x = swap(start_x,end_x)
             if start_y > end_y:
                 start_y,end_y = swap(start_y,end_y)
-            # swap x & y
-            # start_x, start_y = swap(start_x, start_y)
-            # end_x, end_y = swap(end_x, end_y)
             if start_x - end_x == 0:
                 for i in range(start_y, end_y+1):
                     cave[start_x][i] = '#'
@@ -62,12 +55,10 @@ def draw_cave(coords: List[List[List[int]]]) -> List[List[int]]:
                 pass
                 for i in range(start_x, end_x+1):
                     cave[i][start_y] = '#'
-    # done
     return cave
 
 def get_min_max_x_y(coords: List[List[List[int]]]) -> Tuple[int, int, int, int]:
     min_x = sys.maxsize
-    # min_x = 0
     min_y = 0
     max_x = -1
     max_y = -1
@@ -81,7 +72,6 @@ def get_min_max_x_y(coords: List[List[List[int]]]) -> Tuple[int, int, int, int]:
                 min_y = y
             elif y > max_y:
                 max_y = y
-    # max_x, max_y = swap(max_x, max_y)
     print(f"[LOG] min_x = {min_x}, min_y = {min_y}, max_x = {max_x}, max_y = {max_y}")
     return min_x, max_x, min_y, max_y
 

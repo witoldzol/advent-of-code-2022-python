391c4e886753ad65d12b1d28027955fe0fedfc6e
Witold Zolnowski <blue3656@gmail.com>
2023-11-12 08:38:54 +0000

    d14 parse input

--- d14/fast.py
+++ d14/fast.py
@@ -2,3 +2,7 @@ data = open('sample_input').read().strip()
 lines = [x for x in data.split('\n') ]
 for l in lines:
     print(l)
+    for c in l.split('->'):
+        c = c.strip()
+        x,y = c.split(',')
+        x,y = int(x), int(y)

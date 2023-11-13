3235b1d0ca5bdbd886a13fb6509738219d2299fa
Witold Zolnowski <blue3656@gmail.com>
2023-11-13 08:32:02 +0000

    d15,1 parse input

--- d15/15.py
+++ d15/15.py
@@ -1,6 +1,21 @@
-f = open('sample_input', 'r')
-data = f.read().strip()
-lines = data.split('\n')
-for l in lines:
-    print(l)
+import re
+def parse(line:str):
+    pattern = r'.*x=(.*),.*y=(.*):.*x=(.*),.*y=(.*)$'
+    m = re.search(pattern,line)
+    if m:
+        sx,sy,bx,by = m.groups()
+        sx,sy,bx,by = int(sx),int(sy),int(bx),int(by)
+        print(sx,sy,bx,by)
+    else:
+        print('no match')
 
+def main(filename):
+    f = open(filename, 'r')
+    data = f.read().strip()
+    lines = data.split('\n')
+    for l in lines:
+        parse(l)
+
+if __name__ == '__main__':
+    main('input')
+    # main('sample_input')

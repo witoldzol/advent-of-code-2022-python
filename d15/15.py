c64f5297f0976bc488b95d1c2625e2eee43f1619
Witold Zolnowski <blue3656@gmail.com>
2023-11-17 08:23:53 +0000

    d15,1 first stab at generating manhattan lengths, not working propertly

--- d15/15.py
+++ d15/15.py
@@ -15,7 +15,7 @@ def parse_line(line: str) -> Tuple[int,int,int,int]:
     if m:
         sx, sy, bx, by = m.groups()
         sx, sy, bx, by = int(sx), int(sy), int(bx), int(by)
-        log.info(f"sx = {sx}, sy={sy}, bx={bx}, by={by}")
+        log.debug(f"sx = {sx}, sy={sy}, bx={bx}, by={by}")
         return sx, sy, bx, by
     else:
         print("no match")
@@ -34,14 +34,34 @@ def parse_data(filename) -> List[Tuple[int,int,int,int]]:
 
 
 def generate_manhatan_lengths(coords: Tuple[int,int,int,int]):
-    pass
+    m_lenghts = []
+    sx, sy, bx, by = coords
+    dx=abs(sx-bx)
+    dy=abs(sy-by)
+    dd = max(dx,dy)
+    for x in range(0, dd+1):
+        m_lenghts.append((x,dd-x))
+    for x in range(-dd,1):
+        m_lenghts.append((x,dd+x))
+    for y in range(0, dd+1):
+        m_lenghts.append((dd-y,y))
+    for y in range(-dd,1):
+        m_lenghts.append((dd+y,y))
+    return m_lenghts
 
 
 def main(filename):
     C = set()
     coords = parse_data(filename)
-    for c in coords:
+    # for c in coords:
+    lens = {}
+    for c in coords[6:7]:
         m_lens = generate_manhatan_lengths(c)
+        for l in m_lens:
+            x,y = l
+            print(x,y)
+
+
 
 
 

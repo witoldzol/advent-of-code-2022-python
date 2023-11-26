49f1768655549849d26caed2206111e4c022dd95
Witold Zolnowski <blue3656@gmail.com>
2023-11-26 08:00:50 +0000

    d15,1 test fill_the_borders2

--- d15/test_15.py
+++ d15/test_15.py
@@ -1,5 +1,7 @@
 from math import exp
 from typing import Tuple
+# from d15.main import fill_the_borders2
+from main import fill_the_borders2
 from main import generate_manhatan_lengths
 
 
@@ -17,3 +19,13 @@ def test_generate_manhatan_lengths():
     actual = generate_manhatan_lengths(coords)
     expected = [(0, 2), (1, 1), (1, 3), (2, 0), (2, 4), (3, 1), (3, 3), (4, 2)]
     assert sorted(expected) == sorted(actual)
+
+def test_fill_the_borders2():
+    coords = {}
+    m_lenghts = [(1736480, 2000000), (-1042974, 2000000)]
+    fill_the_borders2(coords=coords, lens=m_lenghts)
+    expected = 1736480 - ( -1042974 )
+    assert expected == len(coords)
+    subset_m_lenghts = [(1736480, 2000000), (-104297, 2000000)]
+    fill_the_borders2(coords=coords, lens=subset_m_lenghts)
+    assert expected == len(coords)

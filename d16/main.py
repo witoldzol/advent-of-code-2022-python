eac31ad552d7a1c5f2be939dec055ce64f4c2dfa
Witold Zolnowski <blue3656@gmail.com>
2023-12-19 07:02:29 +0000

    d16,1 add input, sample input

---
+++ d16/main.py
@@ -0,0 +1,13 @@
+def parse_input(filename: str):
+    data = open(filename, 'r').read().strip()
+    lines = data.split('\n')
+    for l in lines:
+        print(l)
+
+def main(input):
+    parse_input(input)
+
+
+if __name__ == '__main__':
+  input = 'sample_input'
+  main(input)

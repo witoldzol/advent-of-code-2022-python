f72c211af4f7ed26b01eef171e9171f6bf97319e
Witold Zolnowski <blue3656@gmail.com>
2023-12-21 08:23:48 +0000

    d16,1 parse input into valves

--- d16/main.py
+++ d16/main.py
@@ -1,12 +1,31 @@
+from typing import List
+
+class Valve():
+    def __init__(self, name: str, rate:int, adjecent: List[str]) -> None:
+        self.adjecent = adjecent
+        self.name = name
+        self.rate = rate
+    def __repr__(self) -> str:
+        return f'{self.name=}, {self.rate=}, {self.adjecent=}'
+
 def parse_input(filename: str):
+    valves = []
     data = open(filename, "r").read().strip()
     lines = data.split("\n")
     for l in lines:
-        print(l)
+        chunks = l.split(' ')
+        name = chunks[1]
+        _,rate = chunks[4].split('=')
+        rate = int(rate[:-1])
+        adjecent = chunks[9:]
+        valves.append(Valve(name,rate,adjecent))
+    return valves
 
 
 def main(input):
-    parse_input(input)
+    valves = parse_input(input)
+    for v in valves:
+        print(v)
 
 
 if __name__ == "__main__":

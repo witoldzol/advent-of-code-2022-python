ca7f057a2793c32f508c949f883ba81f6d102ff1
Witold Zolnowski <blue3656@gmail.com>
2023-12-22 07:32:04 +0000

    d16,1 return tuple not list

--- d16/main.py
+++ d16/main.py
@@ -1,25 +1,28 @@
-from typing import List
+from typing import List, Tuple
 
-class Valve():
-    def __init__(self, name: str, rate:int, adjecent: List[str]) -> None:
+
+class Valve:
+    def __init__(self, name: str, rate: int, adjecent: List[str]) -> None:
         self.adjecent = adjecent
         self.name = name
         self.rate = rate
-    def __repr__(self) -> str:
-        return f'{self.name=}, {self.rate=}, {self.adjecent=}'
 
-def parse_input(filename: str):
+    def __repr__(self) -> str:
+        return f"{self.name=}, {self.rate=}, {self.adjecent=}"
+
+
+def parse_input(filename: str) -> Tuple[Valve]:
     valves = []
     data = open(filename, "r").read().strip()
     lines = data.split("\n")
     for l in lines:
-        chunks = l.split(' ')
+        chunks = l.split(" ")
         name = chunks[1]
-        _,rate = chunks[4].split('=')
+        _, rate = chunks[4].split("=")
         rate = int(rate[:-1])
         adjecent = chunks[9:]
-        valves.append(Valve(name,rate,adjecent))
-    return valves
+        valves.append(Valve(name, rate, adjecent))
+    return tuple(valves)
 
 
 def main(input):

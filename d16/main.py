bfe4ff3a904e15519f1bf23c0a29ac88fc2b0497
Witold Zolnowski <blue3656@gmail.com>
2023-12-23 08:17:39 +0000

    d16,1 Depth First Search, infinite loop, not working, no theory behind it, just pointless code

--- d16/main.py
+++ d16/main.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Dict
 
 
 class Valve:
@@ -8,27 +8,42 @@ class Valve:
         self.rate = rate
 
     def __repr__(self) -> str:
-        return f"{self.name=}, {self.rate=}, {self.adjecent=}"
+        return f"Valve(name={self.name}, rate={self.rate}, adjecent={self.adjecent})"
 
 
-def parse_input(filename: str) -> Tuple[Valve]:
-    valves = []
+def parse_input(filename: str) -> Dict[str,Valve]:
+    valves = {}
     data = open(filename, "r").read().strip()
     lines = data.split("\n")
     for l in lines:
+        print(l)
         chunks = l.split(" ")
         name = chunks[1]
         _, rate = chunks[4].split("=")
         rate = int(rate[:-1])
         adjecent = chunks[9:]
-        valves.append(Valve(name, rate, adjecent))
-    return tuple(valves)
+        adjecent = ' '.join(adjecent)
+        adjecent = adjecent.split(',')
+        adjecent = [x.strip() for x in adjecent]
+        valves[name] = Valve(name, rate, adjecent)
+    print(valves)
+    return valves
+
+def DFS(root: Valve, valves: Dict[str,Valve], count: int):
+    count += 1
+    if count >= 30:
+        return None
+    if not root.adjecent:
+        return None
+    for valve in root.adjecent:
+        print(f'{valve=}')
+        print(f'going from {root=} to {valve=}')
+        DFS(valves[valve], valves, count)
 
 
 def main(input):
     valves = parse_input(input)
-    for v in valves:
-        print(v)
+    DFS(valves['AA'],valves, 0)
 
 
 if __name__ == "__main__":

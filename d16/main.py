12bbadf7cb0cd436c8bea6c1e8f1c7de0243a58b
Witold Zolnowski <blue3656@gmail.com>
2023-12-24 07:57:33 +0000

    d16,1 remove DFS

--- d16/main.py
+++ d16/main.py
@@ -16,7 +16,6 @@ def parse_input(filename: str) -> Dict[str,Valve]:
     data = open(filename, "r").read().strip()
     lines = data.split("\n")
     for l in lines:
-        print(l)
         chunks = l.split(" ")
         name = chunks[1]
         _, rate = chunks[4].split("=")
@@ -26,24 +25,29 @@ def parse_input(filename: str) -> Dict[str,Valve]:
         adjecent = adjecent.split(',')
         adjecent = [x.strip() for x in adjecent]
         valves[name] = Valve(name, rate, adjecent)
-    print(valves)
     return valves
 
-def DFS(root: Valve, valves: Dict[str,Valve], count: int):
-    count += 1
-    if count >= 30:
-        return None
-    if not root.adjecent:
-        return None
-    for valve in root.adjecent:
-        print(f'{valve=}')
-        print(f'going from {root=} to {valve=}')
-        DFS(valves[valve], valves, count)
+# def DFS(root: Valve, valves: Dict[str,Valve], count: int):
+#     count += 1
+#     if count >= 30:
+#         return None
+#     if not root.adjecent:
+#         return None
+#     for valve in root.adjecent:
+#         print(f'{valve=}')
+#         print(f'going from {root=} to {valve=}')
+#         DFS(valves[valve], valves, count)
+
+def print_possible_paths(root: Valve, valves) -> None:
+    for adjecent in root.adjecent:
+        print(adjecent)
 
 
 def main(input):
     valves = parse_input(input)
-    DFS(valves['AA'],valves, 0)
+    for turn in range(1,31):
+        print_possible_paths(valves['AA'], valves)
+
 
 
 if __name__ == "__main__":

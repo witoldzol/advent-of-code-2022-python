0486c4745e50a32cd788a9a191925d5e38ec9366
Witold Zolnowski <blue3656@gmail.com>
2023-12-29 07:59:04 +0000

    d16,1 fix typo

--- d16/main.py
+++ d16/main.py
@@ -3,56 +3,37 @@ from typing import List, Dict, Set, Tuple
 
 class Valve:
     def __init__(self, name: str, rate: int, adjacent: List["Valve"] = None) -> None:
-        self.adjecent = adjacent
+        self.adjacent = adjacent
         self.name = name
         self.rate = rate
         self.visited = False
 
     def __repr__(self) -> str:
-        return f"Valve(name={self.name}, rate={self.rate}, adjecent={[v.name for v in self.adjecent]})"
+        return f"Valve(name={self.name}, rate={self.rate}, adjacent={[v.name for v in self.adjacent]})"
 
 
 def build_valve_graph(filename: str) -> Tuple[Valve, Dict[str,Valve]]:
     valves = {}
     data = open(filename, "r").read().strip()
     lines = data.split("\n")
-    adjecent_dict = {}
+    adjacent_dict = {}
     for l in lines:
         chunks = l.split(" ")
         name = chunks[1]
         _, rate = chunks[4].split("=")
         rate = int(rate[:-1])
-        adjecent = chunks[9:]
-        adjecent = ' '.join(adjecent)
-        adjecent = adjecent.split(',')
-        adjecent = [x.strip() for x in adjecent]
+        adjacent = chunks[9:]
+        adjacent = ' '.join(adjacent)
+        adjacent = adjacent.split(',')
+        adjacent = [x.strip() for x in adjacent]
         valves[name] = Valve(name, rate)
-        adjecent_dict[name] = adjecent
+        adjacent_dict[name] = adjacent
     for valve in valves.values():
-        valve.adjecent = []
-        adjecent_valves = adjecent_dict[valve.name]
-        for name in adjecent_valves:
-            valve.adjecent.append(valves[name])
+        valve.adjacent = []
+        adjacent_valves = adjacent_dict[valve.name]
+        for name in adjacent_valves:
+            valve.adjacent.append(valves[name])
     return valves['AA'], valves
-
-def find_valve(root: Valve, turn: int, target: str, path: List[str] = None) -> List[str] | None:
-    if not path:
-        path = []
-    new_path = path.copy()
-    new_path.append(root.name)
-    root.visited = True
-    if root.name == target:
-        return new_path
-    path.append(root.name)
-    print(f'This is {turn=}, visited {root.name}')
-    turn += 1
-    result = None
-    for valve in root.adjecent:
-        if not valve.visited:
-            result =  find_valve(valve, turn, target, new_path)
-    if result:
-        return result
-
 
 def DFS(root: Valve, target: str, path: str = "") -> str:
     if not root:
@@ -62,7 +43,7 @@ def DFS(root: Valve, target: str, path: str = "") -> str:
     if root.name == target:
         return path
     result = ""
-    for v in root.adjecent:
+    for v in root.adjacent:
         if not v.visited:
             result =  DFS(v, target, path)
     if result:

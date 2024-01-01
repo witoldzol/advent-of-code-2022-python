b321e42cfc867d35acfcdec65c9cc36b5fbc7ce4
Witold Zolnowski <blue3656@gmail.com>
2024-01-01 07:47:00 +0000

    d16,1 calcuate potential returns

--- d16/main.py
+++ d16/main.py
@@ -37,39 +37,43 @@ def build_valve_graph(filename: str) -> Tuple[Valve, Dict[str, Valve]]:
     return valves["AA"], valves
 
 
-def BFS(root: Valve, target: str, map: Dict[str, bool], path: str = "", jumps: int = -1 ) -> Tuple[str, int]:
-    print(f"map has been passed in {map}")
+def BFS(
+    root: Valve,
+    target: str,
+    map: Dict[str, bool] | None = None,
+    path: str = "",
+    jumps: int = -1,
+) -> Tuple[str, int] | None:
+    if not map:
+        map = {}
     if not root:
         return "", jumps
     path += root.name
-    root.visited = True
     map[root.name] = True
     jumps += 1
     if root.name == target:
         return path, jumps
     result = ""
     for v in root.adjacent:
-        if v.visited != map[v.name]:
-            print(f"there is a mismatch between {v.name} = {v.visited} and the map {v.name} = {map[v.name]}")
-        if not v.visited:
-            # print(f"visiting node {root.name}, checking adjecent node {v.name}, in map it is marked as {map[v.name]}")
+        if not v.name in map:
             result = BFS(v, target, map, path, jumps)
             if result:
                 return result
-    return '', -1
 
-def calculate_returns(start: Valve, map: Dict[str, Valve], current_turn: int) -> Dict[str, int]:
-    # pu.db
+
+def calculate_returns(
+    start: Valve, map: Dict[str, Valve], current_turn: int
+    ) -> Dict[str, int]:
     returns_map = {}
     for valve in map.values():
         print(f"{valve=}")
         if valve.name == start.name:
             continue
-        result = BFS(start, valve.name)
-        if not result:
-            raise Exception(f'Unable to find path to valve {valve.name}')
-        _, turns_to_get_to_valve = result
-        turns_to_get_to_valve += 1 # one extra turn to activate the valve
+        path_to_valve = BFS(start, valve.name)
+        if not path_to_valve:
+            raise Exception(f"Unable to find path to valve {valve.name}")
+        _, turns_to_get_to_valve = path_to_valve
+        turns_to_get_to_valve += 1  # one extra turn to activate the valve
         remaining_turns = current_turn - turns_to_get_to_valve
         potential_flow = valve.rate * remaining_turns
         returns_map[valve.name] = potential_flow
--- d16/test.py
+++ d16/test.py
@@ -3,44 +3,50 @@ from main import calculate_returns
 
 
 def test_BFS():
-    map = {'AA': False,'BB': False,'CC': False,'DD': False,'EE': False,}
-    bb = Valve('BB', 10, [])
-    cc = Valve('CC', 10, [])
-    adjacent = [bb,cc]
-    root = Valve('AA', 0, adjacent)
+    map = {
+        "AA": False,
+        "BB": False,
+        "CC": False,
+        "DD": False,
+        "EE": False,
+    }
+    bb = Valve("BB", 10, [])
+    cc = Valve("CC", 10, [])
+    adjacent = [bb, cc]
+    root = Valve("AA", 0, adjacent)
     # one jump
-    actual_path, jumps = BFS(root, 'BB', map.copy())
-    assert 'AABB' == actual_path
+    actual_path, jumps = BFS(root, "BB")
+    assert "AABB" == actual_path
     assert 1 == jumps
-    print(f"END of TEST =======================")
-    dd = Valve('DD', 10, [])
+    dd = Valve("DD", 10, [])
     cc.adjacent.append(dd)
-    ee = Valve('EE', 10, [])
+    ee = Valve("EE", 10, [])
     dd.adjacent.append(ee)
     # two jumps
-    actual_path, jumps = BFS(root, 'EE', map.copy())
-    assert 'AACCDDEE' == actual_path
+    actual_path, jumps = BFS(root, "EE")
+    assert "AACCDDEE" == actual_path
     assert 3 == jumps
     # and again, two jumps using same nodes
-    # actual_path, jumps = BFS(root, 'EE')
-    # assert 'AACCDDEE' == actual_path
-    # assert 3 == jumps
+    actual_path, jumps = BFS(root, "EE")
+    assert "AACCDDEE" == actual_path
+    assert 3 == jumps
 
-# def test_calculate_returns_two_jumps():
-#     aa = Valve('AA', 10, [])
-#     bb = Valve('BB', 10, [])
-#     cc = Valve('CC', 10, [])
-#     dd = Valve('DD', 100, [])
-#     aa_adjacent = [bb,cc]
-#     bb_adjacent = [aa]
-#     cc_adjacent = [aa,dd]
-#     dd_adjacent = [cc]
-#     aa.adjacent = aa_adjacent
-#     bb.adjacent = bb_adjacent
-#     cc.adjacent = cc_adjacent
-#     dd.adjacent = dd_adjacent
-#     map = {"AA": aa, "BB": bb, "CC": cc, "DD": dd}
-#     # calc returns
-#     results_map = calculate_returns(aa, map, 10)
-#     for k,v in results_map.items():
-#         print(k, " = ", v)
+
+def test_calculate_returns_two_jumps():
+    aa = Valve('AA', 10, [])
+    bb = Valve('BB', 10, [])
+    cc = Valve('CC', 10, [])
+    dd = Valve('DD', 100, [])
+    aa_adjacent = [bb,cc]
+    bb_adjacent = [aa]
+    cc_adjacent = [aa,dd]
+    dd_adjacent = [cc]
+    aa.adjacent = aa_adjacent
+    bb.adjacent = bb_adjacent
+    cc.adjacent = cc_adjacent
+    dd.adjacent = dd_adjacent
+    map = {"AA": aa, "BB": bb, "CC": cc, "DD": dd}
+    # calc returns
+    results_map = calculate_returns(aa, map, 10)
+    for k,v in results_map.items():
+        print(k, " = ", v)

32d1aae6e57dfbc390408040f44e3e6e6c97bcbd
Witold Zolnowski <blue3656@gmail.com>
2024-01-18 06:47:28 +0000

    d16,1 clean up unused tests

--- d16/test.py
+++ d16/test.py
@@ -1,7 +1,6 @@
 from typing import OrderedDict, Dict
 from main import Valve_Expected_Returns
-from main import Valve, BFS
-from main import calculate_returns, filter_finished_paths, calculate_returns_for_a_single_turn2, breadth_first_search
+from main import filter_finished_paths, calculate_returns_for_a_single_turn2, breadth_first_search, Valve
 import pytest
 
 @pytest.fixture
@@ -38,47 +37,6 @@ def valve_map():
     map = {"AA": aa, "BB": bb, "CC": cc, "DD": dd, "EE": ee}
     yield map
 
-def test_BFS():
-    map = {
-        "AA": False,
-        "BB": False,
-        "CC": False,
-        "DD": False,
-        "EE": False,
-    }
-    bb = Valve("BB", 10, [])
-    cc = Valve("CC", 10, [])
-    adjacent = [bb, cc]
-    root = Valve("AA", 0, adjacent)
-    # one jump
-    actual_path, jumps = BFS(root, "BB")
-    assert "AABB" == actual_path
-    assert 1 == jumps
-    dd = Valve("DD", 10, [])
-    cc.adjacent.append(dd)
-    ee = Valve("EE", 10, [])
-    dd.adjacent.append(ee)
-    # two jumps
-    actual_path, jumps = BFS(root, "EE")
-    assert "AACCDDEE" == actual_path
-    assert 3 == jumps
-    # and again, two jumps using same nodes
-    actual_path, jumps = BFS(root, "EE")
-    assert "AACCDDEE" == actual_path
-    assert 3 == jumps
-
-
-def test_calculate_returns_two_jumps(valve_map):
-    results_map = calculate_returns(valve_map["AA"], valve_map, 10)
-    assert 700 == results_map["DD"]
-
-
-def test_calculate_returns_three_jumps(valve_map):
-    results_map = calculate_returns(valve_map["AA"], valve_map, 10)
-    assert 700 == results_map["DD"]
-    assert 250 == results_map["EE"]
-
-
 def test_calculate_returns_for_a_single_turn(valve_map):
     er = Valve_Expected_Returns("AA",0,10,"AA",0)
     results = calculate_returns_for_a_single_turn2(er, valve_map)
@@ -87,29 +45,6 @@ def test_calculate_returns_for_a_single_turn(valve_map):
     assert 300 == last.potential_flow
     assert 6 == last.remaining_turns
 
-
-def test_different_paths(valve_map):
-    # generate initial paths
-    er = Valve_Expected_Returns("AA",0,10,"AA",0)
-    results = calculate_returns_for_a_single_turn2(er, valve_map)
-    assert 4 == len(results)
-    last = results[-1]
-    assert 'EE' == last.name
-    assert 300 == last.potential_flow
-    assert 6 == last.remaining_turns
-    # calculate returns for different starts
-    sums = {}
-    for valve in results:
-        od = OrderedDict()
-        od[valve.name] = valve.potential_flow
-        sum = valve.potential_flow
-        path = ''
-        returns: Tuple[Dict[str, int], int] = calculate_returns(valve_map[valve.name], valve_map, valve.remaining_turns, od)
-        for k,v in returns.items():
-            path += k
-            sum += v
-        sums[path] = sum
-    assert sums == {'BBDDEE': 660, 'CCDDEE': 1440, 'DDEECC': 1730, 'EEDDCC': 1080}
 
 def test_finished_path(three_valves):
     print(three_valves)

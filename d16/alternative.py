f8d6a09fd8671cdc18d0c2c7a7a182076ca098ab
Witold Zolnowski <blue3656@gmail.com>
2024-01-22 10:28:31 +0000

    d16,1 parse input

---
+++ d16/alternative.py
@@ -0,0 +1,10 @@
+valves = {}
+tunnels = {}
+
+for line in open("input"):
+    line = line.strip()
+    valve = line.split()[1]
+    flow = int(line.split(";")[0].split("=")[1])
+    targets = line.split("to ")[1].split(" ", 1)[1].split(", ")
+    valves[valve] = flow
+    tunnels[valve] = targets

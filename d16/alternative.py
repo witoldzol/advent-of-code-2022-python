0034c8c87a8c6b7e957502749f0b2e315789ca49
Witold Zolnowski <blue3656@gmail.com>
2024-01-22 11:53:51 +0000

    d16,1 implement bfs to calc distances between nodes

--- d16/alternative.py
+++ d16/alternative.py
@@ -1,10 +1,42 @@
+from typing import Tuple
+from collections import deque
+
 valves = {}
 tunnels = {}
+dists = {}
 
-for line in open("input"):
+def bfs(start: Tuple[int, str], target: str) -> int:
+    queue = deque()
+    queue.append(start)
+    visited = set()
+    while queue:
+        distance, node_name = queue.popleft()
+        if node_name == target:
+            return distance
+        for neighbour in tunnels[node_name]:
+            if neighbour in visited:
+                continue
+            queue.append((distance + 1, neighbour))
+    raise Exception(f"Unable to find a path from {start[1]} to {target}")
+
+# parse input
+input = "input"
+input = "sample_input"
+for line in open(input):
     line = line.strip()
     valve = line.split()[1]
     flow = int(line.split(";")[0].split("=")[1])
     targets = line.split("to ")[1].split(" ", 1)[1].split(", ")
     valves[valve] = flow
     tunnels[valve] = targets
+
+# calculate distances between non zero nodes
+for valve, flow in valves.items():
+    # skip zero nodes
+    if valve != "AA" and flow == 0:
+        continue
+    dists[valve] = {}
+    for v,f in valves.items():
+        if v != "AA" and f == 0 or v == valve:
+            continue
+        dists[valve][v] = bfs((0,valve), v)

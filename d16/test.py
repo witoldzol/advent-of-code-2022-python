14f5c35b8879351a671a5e91090914116e0866b4
Witold Zolnowski <blue3656@gmail.com>
2024-01-24 07:00:01 +0000

    d16 clean up tests

--- d16/test.py
+++ d16/test.py
@@ -1,6 +1,5 @@
-from typing import OrderedDict, Dict
 from main import Valve_Expected_Returns
-from main import filter_finished_paths, calculate_returns_for_a_single_turn, breadth_first_search, Valve, traceback, bfs_print_all_paths
+from main import calculate_returns_for_a_single_turn, breadth_first_search, Valve, traceback, bfs_print_all_paths
 import pytest
 
 @pytest.fixture
@@ -59,41 +58,13 @@ def valve_map_circular():
 
 def test_calculate_returns_for_a_single_turn(valve_map):
     er = Valve_Expected_Returns("AA",0,10,"AA",0)
-    results = calculate_returns_for_a_single_turn(er, valve_map)
+    cache = {}
+    results, cache = calculate_returns_for_a_single_turn(er, valve_map, cache)
     last = results[-1]
     assert 'EE' == last.name
     assert 300 == last.potential_flow
     assert 6 == last.remaining_turns
 
-
-def test_finished_path(three_valves):
-    print(three_valves)
-    er = Valve_Expected_Returns("AA",0,4,"AA",0)
-    results = calculate_returns_for_a_single_turn(er, three_valves)
-    for o in results:
-        print(o)
-    second_run = []
-    for r in results:
-        assert r.finished == False
-        r = calculate_returns_for_a_single_turn(r, three_valves)
-        second_run.extend(r)
-    for r in second_run:
-        assert r.finished == True
-
-
-def test_all_paths(valve_map):
-    finished_paths = []
-    er = Valve_Expected_Returns("AA",0,10,"AA",0)
-    paths = calculate_returns_for_a_single_turn(er, valve_map)
-    wip = []
-    for _ in range(3):
-        for p in wip if wip else paths:
-            pp = calculate_returns_for_a_single_turn(p, valve_map)
-            done, not_done = filter_finished_paths(pp)
-            wip.extend(not_done)
-            finished_paths.extend(done)
-    all_ordered = sorted(finished_paths, key=lambda p: p.total_flow, reverse=True)
-    assert 1120 == all_ordered[0].total_flow
 
 def test_breadth_first_search(valve_map):
     root = valve_map["AA"]
